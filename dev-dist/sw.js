4971b4e00f43a393252ceec2f6b10d4eee1d536f
Valent Vasti <valentvasti2114@gmail.com>
2024-05-30 15:32:36 +0700

    pwa(?)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qsl5miat4mo"
+    "revision": "0.eh6ua1u1mpg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
