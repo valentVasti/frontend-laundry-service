6d353c2df620721ce105e2a26b1e59bbb2e07691
Valent Vasti <valentvasti2114@gmail.com>
2024-06-23 13:05:13 +0700

    bisa gaksi

--- src/server/Url.jsx
+++ src/server/Url.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 
 // export const BASE_URL = 'http://127.0.0.1:8000/api'
 // export const BASE_URL = 'https://tugas-akhir-api.centralus.cloudapp.azure.com/api'
-export const BASE_URL = 'https://wasngoapp'
+export const BASE_URL = 'https://wasngoapp.online'
